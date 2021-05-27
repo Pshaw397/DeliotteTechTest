bea8889d6325616a40a28f4b35cf4c7b8b152fa2
Pshaw397 <phillip.a.shaw@googlemail.com>
2021-05-27 12:06:17 +0100

    Added pling tests

--- TechTest/RaindropTests/UnitTest1.cs
+++ TechTest/RaindropTests/UnitTest1.cs
@@ -1,13 +1,17 @@
 using NUnit.Framework;
+using TechTest;
 
 namespace RaindropTests
 {
     public class Tests
     {
-        [Test]
-        public void Test1()
+        [TestCase(3, "Pling")]
+        [TestCase(6, "Pling")]
+        [TestCase(9, "Pling")]
+        [TestCase(12, "Pling")]
+        public void AFactorOf3IsInput_OutputIsPling(int input, string result)
         {
-            Assert.Pass();
+            Assert.That(Program.NumConvert(input), Is.EqualTo(result));
         }
     }
 }
